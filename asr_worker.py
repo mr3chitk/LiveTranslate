19aca514a3c193394102f59f6256bbdfb35d2dd6
Test Account <mr3chitk@gmail.com>
2026-09-08 22:01:03 +0700

    Fix bug

--- asr_worker.py
+++ asr_worker.py
@@ -95,7 +95,7 @@ def _load_engine(config: dict):
             language=language,
             download_root=download_root,
             pad_seconds=pad_seconds,
-            hot_words=config.get("hot_words","")
+            hot_words=config.get("hot_words",""),
             init_prompt=config.get("init_prompt","")
         )
 
